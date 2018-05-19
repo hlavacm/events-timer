0ea0aa6366617b2e0de1836703429720ba2f36c4
Martin Hlaváč <hlavacm@hotmail.com>
2018-05-19 12:04:08 +0200

    Builded new version to /docs (2.0.2)

--- src/components/FontSizer.vue
+++ src/components/FontSizer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="field is-grouped" v-show="fontSizerEnabled">
+  <div class="field is-grouped is-hidden-mobile" v-show="fontSizerEnabled">
     <p class="control">
       <control-button title="" :colorClass="$parent.$parent.colorClass" @clicked="zoomIn" :disabled="zoomInEnabled">
         <svg width="20px" height="20px" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" class="font-size" v-bind:class="{ 'is-light': isLightFontColor, 'is-dark': isDarkFontColor }"><path d="M417.4,224H288V94.6c0-16.9-14.3-30.6-32-30.6c-17.7,0-32,13.7-32,30.6V224H94.6C77.7,224,64,238.3,64,256  c0,17.7,13.7,32,30.6,32H224v129.4c0,16.9,14.3,30.6,32,30.6c17.7,0,32-13.7,32-30.6V288h129.4c16.9,0,30.6-14.3,30.6-32  C448,238.3,434.3,224,417.4,224z"/></svg>
